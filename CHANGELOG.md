692342683c1fabd2330b43e331f5711f85aa337b
Steven Hammon <steve@hammon.com>
2025-11-26 11:49:04 -0600

    added install script (iOS/Linux)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
 
+## [v0.1.1] - 2025-11-16
+
+- Created install script (iOS/Linux)
+
 ## [v0.1.0] - 2025-11-26
 
 - Add changelog to project
@@ -15,4 +19,5 @@ adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 - Removed insecure connection config in Client
 
 [Unreleased] https://github.com/s-hammon/agfapi/compare/v0.1.0...HEAD
+[v0.1.1] https://github.com/s-hammon/agfapi/compare/v0.1.0...v0.1.1
 [v0.1.0]: https://github.com/s-hammon/agfapi/releases/tag/v0.1.0
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Adapted from the Deno installer: Copyright 2019 the Deno authors. All rights reserved. MIT license.
+# Ref: https://github.com/denoland/deno_install
+# TODO(everyone): Keep this script simple and easily auditable.
+
+# TODO(mf): this should work on Linux and macOS. Not intended for Windows.
+
+set -e
+
+os=$(uname -s | tr '[:upper:]' '[:lower:]')
+arch=$(uname -m)
+
+if [ "$arch" = "aarch64" ]; then
+	arch="arm64"
+fi
+
+if [ $# -eq 0 ]; then
+	agfapi_uri="https://github.com/s-hammon/agfapi/releases/latest/download/agfapi_${os}_${arch}"
+else
+	agfapi_uri="https://github.com/s-hammon/agfapi/releases/download/${1}/agfapi_${os}_${arch}"
+fi
+
+agfapi_install="${AGFAPI_INSTALL:-/usr/local}"
+bin_dir="${agfapi_install}/bin"
+exe="${bin_dir}/agfapi"
+
+if [ ! -d "${bin_dir}" ]; then
+	mkdir -p "${bin_dir}"
+fi
+
+curl --sielnt --show-error --location --fail --location --output "${exe}" "$agfapi_uri"
+chmod +x "${exe}"
+
+echo "agfapi was installed successfully to ${exe}"
+if command -v agfapi >/dev/null; then
+	echo "Run 'agfapi --help' to get started"
+fi
